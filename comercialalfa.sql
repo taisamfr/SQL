edd164cf171b3d33bf922195ec4783799403ceba
taisinha <taisa.reis1a@gmail.com>
2025-09-24 14:18:16 -0300

    rh.sql

--- comercialalfa.sql
+++ comercialalfa.sql
@@ -1,9 +1,9 @@
--- phpMyAdmin SQL Dump
+L Dump
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 24/09/2025 às 16:20
+-- Tempo de geração: 24/09/2025 às 16:35
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -18,137 +18,125 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `comercialalfa`
+-- Banco de dados: rh
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `compra`
+-- Estrutura para tabela departamento
 --
 
-CREATE TABLE `compra` (
-  `id` int(11) NOT NULL,
-  `id_produto` int(11) DEFAULT NULL,
-  `data_pedido` date DEFAULT NULL,
-  `valor_total` double DEFAULT NULL
+CREATE TABLE departamento (
+  ID int(11) NOT NULL,
+  nome varchar(50) NOT NULL,
+  localizacao int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Despejando dados para a tabela `compra`
+-- Despejando dados para a tabela departamento
 --
 
-INSERT INTO `compra` (`id`, `id_produto`, `data_pedido`, `valor_total`) VALUES
-(1, 2, '2025-09-22', 25),
-(2, 1, '2025-09-22', 507),
-(3, 3, '2025-09-22', 75),
-(4, 5, '2025-09-22', 45),
-(5, 4, '2025-09-22', 15);
+INSERT INTO departamento (ID, nome, localizacao) VALUES
+(1, 'adm', 18200880),
+(2, 'educação', 18200880),
+(3, 'saude', 18200880),
+(4, 'manutencao', 18200880),
+(5, 'limpeza', 18200880);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `fornecedor`
+-- Estrutura para tabela funcionario
 --
 
-CREATE TABLE `fornecedor` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(100) NOT NULL,
-  `cnpj` varchar(11) DEFAULT NULL,
-  `telefone` varchar(20) DEFAULT NULL,
-  `cidade` varchar(100) DEFAULT NULL
+CREATE TABLE funcionario (
+  ID int(11) NOT NULL,
+  nome varchar(50) NOT NULL,
+  cpf int(11) NOT NULL,
+  data_admissao double NOT NULL,
+  cargo varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Despejando dados para a tabela `fornecedor`
+-- Despejando dados para a tabela funcionario
 --
 
-INSERT INTO `fornecedor` (`id`, `nome`, `cnpj`, `telefone`, `cidade`) VALUES
-(1, 'João Vitor O.', '12345678912', '4444-5555', 'Sorocaba'),
-(2, 'Ana Clara', '3216549741', '1111-2222', ' Votorantim'),
-(3, 'José Oliveira', '78945612302', '1111-2222', 'Salto de Pirapora'),
-(4, 'Olivia Camargo', '45698712301', '7777-8888', 'Piedade'),
-(5, 'Isabel Pedroso', '23654789101', '9999-8888', 'Araçoiaba');
+INSERT INTO funcionario (ID, nome, cpf, data_admissao, cargo) VALUES
+(1, 'renan', 19021315, 2025, 'prof'),
+(2, 'juan', 161671, 2025, 'ajudante'),
+(3, 'thiago', 437378, 2025, 'gerente'),
+(4, 'fichel', 31526737, 2025, 'cozinheira'),
+(5, 'lucca', 638891, 2025, 'ceo');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `produto`
+-- Estrutura para tabela treinamento
 --
 
-CREATE TABLE `produto` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(100) NOT NULL,
-  `categoria` varchar(100) DEFAULT NULL,
-  `preco` double(10,2) DEFAULT NULL,
-  `quantidade` int(11) DEFAULT NULL
+CREATE TABLE treinamento (
+  ID int(11) NOT NULL,
+  titulo varchar(50) NOT NULL,
+  data_treino double NOT NULL,
+  carga_horaria double NOT NULL,
+  id_funcionario int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Despejando dados para a tabela `produto`
+-- Despejando dados para a tabela treinamento
 --
 
-INSERT INTO `produto` (`id`, `nome`, `categoria`, `preco`, `quantidade`) VALUES
-(1, 'detergente', 'limpeza', 2.00, 10),
-(2, 'arroz', 'alimentacao', 25.00, 20),
-(3, 'shampoo', 'beleza', 15.00, 5),
-(4, 'macarrão', 'alimentacao', 5.00, 9),
-(5, 'chocolate', 'alimentacao', 3.00, 5);
+INSERT INTO treinamento (ID, titulo, data_treino, carga_horaria, id_funcionario) VALUES
+(1, 'salva vidas', 2025, 8, 2),
+(2, 'power bi', 2025, 12, 1),
+(3, 'excel', 2025, 2, 3),
+(4, 'java', 2025, 5, 4),
+(5, 'html', 2025, 7, 5);
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices de tabela `compra`
+-- Índices de tabela departamento
 --
-ALTER TABLE `compra`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_produto` (`id_produto`);
+ALTER TABLE departamento
+  ADD PRIMARY KEY (ID);
 
 --
--- Índices de tabela `fornecedor`
+-- Índices de tabela funcionario
 --
-ALTER TABLE `fornecedor`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE funcionario
+  ADD PRIMARY KEY (ID);
 
 --
--- Índices de tabela `produto`
+-- Índices de tabela treinamento
 --
-ALTER TABLE `produto`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE treinamento
+  ADD PRIMARY KEY (ID);
 
 --
 -- AUTO_INCREMENT para tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT de tabela `compra`
+-- AUTO_INCREMENT de tabela departamento
 --
-ALTER TABLE `compra`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE departamento
+  MODIFY ID int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT de tabela `fornecedor`
+-- AUTO_INCREMENT de tabela funcionario
 --
-ALTER TABLE `fornecedor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE funcionario
+  MODIFY ID int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT de tabela `produto`
+-- AUTO_INCREMENT de tabela treinamento
 --
-ALTER TABLE `produto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- Restrições para tabelas despejadas
---
-
---
--- Restrições para tabelas `compra`
---
-ALTER TABLE `compra`
-  ADD CONSTRAINT `compra_ibfk_1` FOREIGN KEY (`id_produto`) REFERENCES `produto` (`id`) ON DELETE CASCADE;
+ALTER TABLE treinamento
+  MODIFY ID int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
